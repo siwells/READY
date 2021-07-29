3fe42574be2a1b626e56822a066991c9e3485dcd
Simon Wells <siwells@gmail.com>
2021-07-29 20:58:53 +0100

    Update due to deprecated use of oscillator.frequency.value

--- examples/08_audio-theremin.js
+++ examples/08_audio-theremin.js
@@ -4,5 +4,5 @@ h = window.innerHeight;
 oscillator.connect(context.destination);
 oscillator.start(0);
 document.addEventListener("mousemove", function(e) {
-    oscillator.frequency.value = e.clientY / h * 1000 + 300;
+    oscillator.frequency.setValueAtTime(e.clientY / h * 1000 + 300, context.currentTime);
 });
